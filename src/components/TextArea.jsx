d3cab1e80ec4f11adc1db9aa1792d63ba5dedfa8
PiyushSonawane1302 <piyushsonawane4455@gmail.com>
2021-09-26 10:48:50 +0530

    Text-Utils

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,60 +1,71 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import PreviewArea from './PreviewArea';
 
 export default function TextForm(props) {
 
-    const [text,setText] = useState('');
-    const [preview,setPreview] = useState('');
+    const [text, setText] = useState('');
+    const [preview, setPreview] = useState('');
 
-    function handleUppercase(){
+    function handleUppercase() {
         let upperText = text.toUpperCase();
         setPreview(upperText)
     }
 
-    function handleLowercase(){
+    function handleLowercase() {
         let lowerText = text.toLowerCase();
         setPreview(lowerText);
     }
 
-    function handleCleartext(){
+    function handleCleartext() {
         let newText = ""
         setText(newText)
     }
 
-    function handleRemoveSpaces(){
-        let newText =  text.replace(/\s+/g, ' ').trim()
+    function handleRemoveSpaces() {
+        let newText = text.replace(/\s+/g, ' ').trim()
         setPreview(newText);
         setText(newText);
     }
 
 
-    function handleOnChange(e){
+    function handleOnChange(e) {
         const text = e.target.value;
         setText(text)
     }
 
     return (
         <>
-        <div className="container">
-            <div className="mt-5 mb-3">
-                <label style={{backgroundColor:props.mode==='dark'?'#1c2a35':''}} htmlFor="mytextbox" className={`form-label badge text-wrap ${props.mode==='light'?'bg-dark':''} p-2 ms-2 fs-6`}>Enter your Text here 👇</label>
-                <textarea style={{backgroundColor: props.mode==='dark'?'#1c2a35':'',border:props.mode==='dark'?'none':'',color:props.mode==='dark'?'#fff':''}} onChange={handleOnChange} placeholder={"Enter Text ✍"} value={text} className="form-control mt-2" id="mytextbox" rows="6"></textarea>
+            <div className="container">
+                <div className="mt-5 mb-3">
+                    <label style={{ backgroundColor: props.mode === 'dark' ? '#1c2a35' : '' }} htmlFor="mytextbox" className={`form-label badge text-wrap ${props.mode === 'light' ? 'bg-dark' : ''} p-2 ms-2 fs-6`}>Enter your Text here 👇</label>
+                    <textarea style={{ backgroundColor: props.mode === 'dark' ? '#1c2a35' : '', border: props.mode === 'dark' ? 'none' : '', color: props.mode === 'dark' ? '#fff' : '' }} onChange={handleOnChange} placeholder={"Enter Text ✍"} value={text} className="form-control mt-2" id="mytextbox" rows="6"></textarea>
+                </div>
+                <div className="row">
+                    <div className="col col-lg-3 col-md-6 col-sm-12">
+                        <button style={{ backgroundColor: props.mode === 'dark' ? '#1c2a35' : '' }} onClick={handleUppercase} className="btn btn-dark">Upper case</button>
+                    </div>
+                    <div className="col col-lg-3 col-md-6 col-sm-12">
+                        <button style={{ backgroundColor: props.mode === 'dark' ? '#1c2a35' : '' }} onClick={handleLowercase} className="btn btn-dark ms-2">Lower case</button>
+                    </div>
+                    <div className="col col-lg-3 col-md-6 col-sm-12">
+                        <button style={{ backgroundColor: props.mode === 'dark' ? '#1c2a35' : '' }} onClick={handleRemoveSpaces} className="btn btn-dark ms-2">Remove Extra Spaces</button>
+                    </div>
+                    <div className="col col-lg-3 col-md-6 col-sm-12">
+                        <button style={{ backgroundColor: props.mode === 'dark' ? '#1c2a35' : '' }} onClick={handleCleartext} className="btn btn-dark ms-2">Clear</button>
+                    </div>
+
+                </div>
+
             </div>
-            <button style={{backgroundColor:props.mode==='dark'?'#1c2a35':''}} onClick={handleUppercase} className="btn btn-dark">Upper case</button>
-            <button style={{backgroundColor:props.mode==='dark'?'#1c2a35':''}} onClick={handleLowercase} className="btn btn-dark ms-2">Lower case</button>
-            <button style={{backgroundColor:props.mode==='dark'?'#1c2a35':''}} onClick={handleRemoveSpaces} className="btn btn-dark ms-2">Remove Extra Spaces</button>
-            <button style={{backgroundColor:props.mode==='dark'?'#1c2a35':''}} onClick={handleCleartext} className="btn btn-dark ms-2">Clear</button>
-        </div>
 
-        <div className="container">
-            <h5 className="mt-5 mb-4 fw-bold">Text Summary</h5>
-            <p>Words : {text.split(" ").length-1} Characters : {text.length}</p>
-            <p>Time to read : { text.split(" ").length-1>0 ? 0.008 * text.split(" ").length : 0}</p>
-        </div>
+            <div className="container">
+                <h5 className="mt-5 mb-4 fw-bold">Text Summary</h5>
+                <p>Words : {text.split(" ").length - 1} Characters : {text.length}</p>
+                <p>Time to read : {text.split(" ").length - 1 > 0 ? 0.008 * text.split(" ").length : 0}</p>
+            </div>
 
-        <PreviewArea textopt={preview} mode={props.mode} setAlertState={props.setAlertState}/>
+            <PreviewArea textopt={preview} mode={props.mode} setAlertState={props.setAlertState} />
 
-        
         </>
     )
 }
